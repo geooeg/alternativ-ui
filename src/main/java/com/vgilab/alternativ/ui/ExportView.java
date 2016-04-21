93bb08d82094cc76f7eee042649be62e4d324047
Sascha Müllner <sascha.muellner@gmail.com>
2016-04-21 09:54:20 +0200

    Added date formatting

--- src/main/java/com/vgilab/alternativ/ui/ExportView.java
+++ src/main/java/com/vgilab/alternativ/ui/ExportView.java
@@ -7,7 +7,9 @@ import java.util.LinkedList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
@@ -28,7 +30,9 @@ public class ExportView {
                 final ReportItem report = new ReportItem();
                 report.setTripId(curAlterNativ.getId());
                 report.setUserId(curAlterNativ.getUserId());
-                report.setTripStartTime(curAlterNativ.getCreatedAt());
+                final DateTime createdAt = new DateTime(curAlterNativ.getCreatedAt());
+                final DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("dd/MM/yyyyHH:mm:ss");
+                report.setTripStartTime(dateTimeFormatter.print(createdAt));
                 report.setPrimaryModeChoosen(curAlterNativ.getChosenType());
                 report.setTripStartLocation(curAlterNativ.getOrigin().getAddress());
                 report.setTripEndLocation(curAlterNativ.getDestination().getAddress());
